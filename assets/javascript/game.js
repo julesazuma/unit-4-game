dcb9dfc5d18434bfb7331503e137070eca17a6fd
PANDAmonium <jazuma86@yahoo.com>
2018-09-05 21:21:15 -0700

    added global variables to javascript page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
 
+var computerChoice = Math.floor(Math.random()*(120 - 19 + 1) + 19);
+var winCount = 0;
+var loseCount = 0;
+var diamondChoice = Math.floor(Math.random()*(12 - 1 + 1) + 1);
+var rubyChoice = Math.floor(Math.random()*(12 - 1 + 1) + 1);
+var emeraldChoice = Math.floor(Math.random()*(12 - 1 + 1) + 1);
+var sapphireChoice = Math.floor(Math.random()*(12 - 1 + 1) + 1);
+var userTotalScore = 0;
+
+
+});
