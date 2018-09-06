19056c246f58b4923a70a3cbc744cd61b93d9396
PANDAmonium <jazuma86@yahoo.com>
2018-09-06 00:44:04 -0700

    added functions for crystal values and gameplay

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,8 +6,41 @@ var loseCount = 0;
 var diamondChoice = Math.floor(Math.random()*(12 - 1 + 1) + 1);
 var rubyChoice = Math.floor(Math.random()*(12 - 1 + 1) + 1);
 var emeraldChoice = Math.floor(Math.random()*(12 - 1 + 1) + 1);
-var sapphireChoice = Math.floor(Math.random()*(12 - 1 + 1) + 1);
+var citrineChoice = Math.floor(Math.random()*(12 - 1 + 1) + 1);
 var userTotalScore = 0;
 
 
+function crystalValue(){
+	diamondChoice = Math.floor(Math.random()*(12 - 1 + 1) + 1);
+	rubyChoice = Math.floor(Math.random()*(12 - 1 + 1) + 1);
+	emeraldChoice = Math.floor(Math.random()*(12 - 1 + 1) + 1);
+	citrineChoice = Math.floor(Math.random()*(12 - 1 + 1) + 1);
+}
+
+
+function gamePlay(){
+	if(userTotalScore === computerChoice){
+		winCount++;
+		$('#winCounter').text(winCount);
+		$('#winLose').text('You win!');
+		computerChoice = Math.floor(Math.random()*(120 - 19 + 1) + 19);
+		$('#randomNumber').text(computerChoice);
+		userTotalScore = 0;
+		$('#currentScoreCounter').text(userTotalScore);
+		crystalValue();
+	} else if(userTotalScore > computerChoice){
+		loseCount++;
+		$('#loss-counter').html(loseCount);
+		$('#winLose').text('You lose!');
+		computerChoice = Math.floor(Math.random()*(120 - 19 + 1) + 19);
+		$('#randomNumber').text(computerChoice);
+		userTotalScore = 0;
+		$('#currentScoreCounter').text(userTotalScore);
+		crystalValue();
+	}
+}
 });
+
+
+
+
